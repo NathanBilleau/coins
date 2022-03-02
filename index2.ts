9789c7d5a3c47c8212f8f8ff681250e2658c2378
Nathan <nbilleau@gmail.com>
2022-03-02 16:30:47 +0100

    min and max variable

--- index2.ts
+++ index2.ts
@@ -1,7 +1,9 @@
 {
     const stack = 50
-    if (stack > 1) {
-        const first = (stack - 1) % 6 || 5
+    const min = 1
+    const max = 5
+    if (stack > 1 && !((min + max + 1) === stack)) {
+        const first = (stack - 1) % (min + max) || max
         console.log(`alti plays ${first} and wins`)
     }
     else {
